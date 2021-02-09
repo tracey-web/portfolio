6ad489149bd70603bd6d5b502ea41a524def7cf6
Tracey <tracey@coch.pro>
2021-02-09 12:51:15 +1100

    Project copy changes to headings and spelling.

--- src/mock/data.js
+++ src/mock/data.js
@@ -29,7 +29,7 @@ export const projectsData = [
   {
     id: nanoid(),
     img: 'markedit.png',
-    title: 'MarkedIt',
+    title: 'MarkedIt - Frontend & Design',
     info: `It is Markedit's mission is to simplify and streamline the assessment, marking and reporting process to save educators and teachers time. We are an Australian team working with qualified teachers to deliver an automated and Simplified Marking Service (SMS).`,
     info2: `Founded in August 2017 we aim to be the world leader in teaching tools for streamlining the student assessment, marking and reporting processes.`,
     url: 'https://markedit.io/',
@@ -38,8 +38,8 @@ export const projectsData = [
   {
     id: nanoid(),
     img: 'coch-pro.png',
-    title: 'Coch Pro',
-    info: 'A coaching marketplace ofr the mastery of personal development.',
+    title: 'Coch Pro - Frontend & Design',
+    info: 'A coaching marketplace for the mastery of personal development.',
     info2:
       'Social media influencers, senior business executives and art masters alike register on this platform to provide communities for their followers. This unique monetisation model allows content owners to monetise their passion through fans and registered community members who sign up to get personalised advice, professional coaching and mentoring experiences.',
     url: 'https://coch.pro/',
@@ -48,7 +48,7 @@ export const projectsData = [
   {
     id: nanoid(),
     img: 'mixr.png',
-    title: 'mixr',
+    title: 'mixr - Full-stack & Design',
     info: `React / Gatsby App that makes recipies from fridge ingredients.`,
     info2: `The app polls a refigerator API and a recipe API and provides a list of recipe options based on the ingredients available and the recipies it finds.`,
     url: '',
@@ -57,7 +57,7 @@ export const projectsData = [
   {
     id: nanoid(),
     img: 'traceyleastudio.png',
-    title: 'Tracey Lea Studio',
+    title: 'Tracey Lea Studio - Frontend & Design',
     info: `Artist website and portfolio - Designs, artworks and media from Tracey's art and exhibition catalogue.`,
     info2: `Tracey has an extensive academic profile in fine arts. In 2017 she completed a Bachelor of Contemporary Arts with First-Class Honours, which resulted in her acceptance as a PhD Candidate (Creative Arts) which she undertook with the University of Tasmania. Key themes that can be seen in Tracey's work are: perception, actual/virtual place, landscape, digital engagement, voice and sanitisation. `,
     url: 'https://www.traceylea.com',
