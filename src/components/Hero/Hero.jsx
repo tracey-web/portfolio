a7ed3714c0a8adeb57e1b935cb5c6a6280e7b61f
Tracey <tracey@coch.pro>
2021-02-09 21:27:32 +1100

    final changes before live to site

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -2,7 +2,10 @@ import React, { useContext, useState, useEffect } from 'react';
 import { Container } from 'react-bootstrap';
 import Fade from 'react-reveal/Fade';
 import { Link } from 'react-scroll';
+import VideoBg from 'reactjs-videobg';
 import PortfolioContext from '../../context/context';
+import mp4 from '../../images/clouds.mp4';
+import poster from '../../images/clouds.png';
 
 const Header = () => {
   const { hero } = useContext(PortfolioContext);
@@ -23,20 +26,23 @@ const Header = () => {
 
   return (
     <section id="hero" className="jumbotron">
+      <VideoBg poster={poster} wrapperClass="cloud-video">
+        <VideoBg.Source src={mp4} type="video/mp4" />
+      </VideoBg>
       <Container>
         <Fade left={isDesktop} bottom={isMobile} duration={1000} delay={500} distance="30px">
           <h1 className="hero-title">
             {title || 'Hi, my name is'}{' '}
-            <span className="text-color-main">{name || 'Your Name'}</span>
+            <span className="text-color-main">{name || 'Tracey Hill'}</span>.
             <br />
-            {subtitle || "I'm the Unknown Developer."}
+            {subtitle || "I'm a software developer."}
           </h1>
         </Fade>
         <Fade left={isDesktop} bottom={isMobile} duration={1000} delay={1000} distance="30px">
           <p className="hero-cta">
             <span className="cta-btn cta-btn--hero">
               <Link to="about" smooth duration={1000}>
-                {cta || 'Know more'}
+                {cta || 'Learn more'}
               </Link>
             </span>
           </p>
--- src/mock/data.js
+++ src/mock/data.js
@@ -21,45 +21,55 @@ export const aboutData = {
   paragraphOne: '',
   paragraphTwo: '',
   paragraphThree: '',
-  resume: 'https://www.resumemaker.online/es.php', // if no resume, the button will not show up
+  resume: 'http://bit.ly/3tCJDfS', // if no resume, the button will not show up
 };
 
 // PROJECTS DATA
 export const projectsData = [
   {
     id: nanoid(),
-    img: 'project.jpg',
-    title: '',
-    info: '',
-    info2: '',
-    url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+    img: 'markedit.png',
+    title: 'MarkedIt - Frontend & Design',
+    info: `It is Markedit's mission is to simplify and streamline the assessment, marking and reporting process to save educators and teachers time. We are an Australian team working with qualified teachers to deliver an automated and Simplified Marking Service (SMS).`,
+    info2: `Founded in August 2017 we aim to be the world leader in teaching tools for streamlining the student assessment, marking and reporting processes.`,
+    url: 'https://markedit.io/',
+    repo: '', // if no repo, the button will not show up
   },
   {
     id: nanoid(),
-    img: 'project.jpg',
-    title: '',
-    info: '',
-    info2: '',
-    url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+    img: 'coch-pro.png',
+    title: 'Coch Pro - Frontend & Design',
+    info: 'A coaching marketplace for the mastery of personal development.',
+    info2:
+      'Social media influencers, senior business executives and art masters alike register on this platform to provide communities for their followers. This unique monetisation model allows content owners to monetise their passion through fans and registered community members who sign up to get personalised advice, professional coaching and mentoring experiences.',
+    url: 'https://coch.pro/',
+    repo: '', // if no repo, the button will not show up
   },
   {
     id: nanoid(),
-    img: 'project.jpg',
-    title: '',
-    info: '',
-    info2: '',
+    img: 'mixr.png',
+    title: 'mixr - Full-stack & Design',
+    info: `React / Gatsby App that makes recipies from fridge ingredients.`,
+    info2: `The app polls a refigerator API and a recipe API and provides a list of recipe options based on the ingredients available and the recipies it finds.`,
     url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+    repo: '', // if no repo, the button will not show up
+  },
+  {
+    id: nanoid(),
+    img: 'traceyleastudio.png',
+    title: 'Tracey Lea Studio - Frontend & Design',
+    info: `Artist website and portfolio - Designs, artworks and media from Tracey's art and exhibition catalogue.`,
+    info2: `Tracey has an extensive academic profile in fine arts. In 2017 she completed a Bachelor of Contemporary Arts with First-Class Honours, which resulted in her acceptance as a PhD Candidate (Creative Arts) which she undertook with the University of Tasmania. Key themes that can be seen in Tracey's work are: perception, actual/virtual place, landscape, digital engagement, voice and sanitisation. `,
+    url: 'https://www.traceylea.com',
+    repo: '', // if no repo, the button will not show up
   },
 ];
 
 // CONTACT DATA
 export const contactData = {
-  cta: '',
-  btn: '',
-  email: '',
+  cta: 'Interested in working together?',
+  btn: 'Ping Me',
+  email: 'tracey@traceylea.com',
 };
 
 // FOOTER DATA
@@ -68,22 +78,22 @@ export const footerData = {
     {
       id: nanoid(),
       name: 'twitter',
-      url: '',
+      url: 'https://twitter.com/DigitalPye',
     },
     {
       id: nanoid(),
       name: 'codepen',
-      url: '',
+      url: 'https://codepen.io/digitalPye',
     },
     {
       id: nanoid(),
       name: 'linkedin',
-      url: '',
+      url: 'https://www.linkedin.com/in/tracey-hill-01152824/',
     },
     {
       id: nanoid(),
       name: 'github',
-      url: '',
+      url: 'https://github.com/tracey-web',
     },
   ],
 };
