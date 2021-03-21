2705976d075d6edb62a9f1c8ace5b928f2d671c2
Tracey <tracey@coch.pro>
2021-03-21 22:41:53 +1100

    Preview image added to header info.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,6 @@
 import React, { useState, useEffect } from 'react';
+import { Helmet } from 'react-helmet';
+import { useStaticQuery, graphql } from 'gatsby';
 import Hero from './Hero/Hero';
 import About from './About/About';
 import Projects from './Projects/Projects';
@@ -9,12 +11,24 @@ import { PortfolioProvider } from '../context/context';
 
 import { heroData, aboutData, projectsData, contactData, footerData } from '../mock/data';
 
+const query = graphql`
+  query SEO {
+    site {
+      siteMetadata {
+        image
+      }
+    }
+  }
+`;
+
 function App() {
   const [hero, setHero] = useState({});
   const [about, setAbout] = useState({});
   const [projects, setProjects] = useState([]);
   const [contact, setContact] = useState({});
   const [footer, setFooter] = useState({});
+  const { site } = useStaticQuery(query);
+  const { image } = site.siteMetadata;
 
   useEffect(() => {
     setHero({ ...heroData });
@@ -26,6 +40,14 @@ function App() {
 
   return (
     <PortfolioProvider value={{ hero, about, projects, contact, footer }}>
+      <Helmet>
+        <meta property="og:title" content="Tracey Hill - Developer" />
+        {!!image && <meta property="og:image" content={image} />}
+        <meta
+          property="og:description"
+          content="A software developer and educator with 10+ years experience in web technologies. Focusing on advancing my skills as a full-stack developer with the addition of UI and UX design skills."
+        />
+      </Helmet>
       <Hero />
       <About />
       <Projects />
