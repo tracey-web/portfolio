17606289b77532ae187278797d1e09a6ef16760e
Tracey <tracey@coch.pro>
2021-11-06 22:40:07 +1100

    updated headline

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -35,7 +35,7 @@ const Header = () => {
             {title || 'Hi, my name is'}{' '}
             <span className="text-color-main">{name || 'Tracey Hill'}</span>.
             <br />
-            {subtitle || "I'm a software developer."}
+            {subtitle || "I'm a software engineer."}
           </h1>
         </Fade>
         <Fade left={isDesktop} bottom={isMobile} duration={1000} delay={1000} distance="30px">
